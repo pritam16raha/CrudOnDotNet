a9eef55889b6d496a87f6b8fc9006b6e836cd699
rahapritam32 <rahapritam32@gmail.com>
2024-10-05 10:28:41 +0530

    first commit

---
+++ CrudOnMySQL/Controllers/EmployeeController.cs
@@ -0,0 +1,95 @@
+﻿using CrudOnMySQL.Data;
+using CrudOnMySQL.Models;
+using CrudOnMySQL.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudOnMySQL.Controllers
+{
+    [Route("api/[controller]")] //localhost:port/api/[controller]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeeController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        //getting all data
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            //var allEmployees = dbContext.Employees.ToList();
+            //return Ok(allEmployees);
+            return Ok(dbContext.Employees.ToList());
+        }
+
+        //get single user data
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id)
+        {
+            var userInDb = dbContext.Employees.Find(id);
+            if (userInDb is null)
+            {
+                return NotFound();
+            }
+            return Ok(userInDb);
+        }
+
+        //posting single data
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDTO addEmployeeDTO) //DTO -> Data Transfer Object
+        {
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDTO.Name,
+                Email = addEmployeeDTO.Email,
+                Phone = addEmployeeDTO.Phone,
+                Salary = addEmployeeDTO.Salary,
+                Photo = addEmployeeDTO.Photo,
+            };
+            dbContext.Employees.Add(employeeEntity);
+            dbContext.SaveChanges();
+            return Ok(employeeEntity);
+        }
+
+        //update single user
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult updateEmployee(Guid id, UpdateEmployeeDTO updateEmployeeDTO)
+        {
+            var userInDb = dbContext.Employees.Find(id);
+            if (userInDb is null)
+            {
+                return NotFound();
+            }
+
+            userInDb.Name = updateEmployeeDTO.Name;
+            userInDb.Email = updateEmployeeDTO.Email;
+            userInDb.Phone = updateEmployeeDTO.Phone;
+            userInDb.Salary = updateEmployeeDTO.Salary;
+            userInDb.Photo = updateEmployeeDTO.Photo;
+
+            dbContext.SaveChanges();
+            return Ok(userInDb);
+        }
+
+        //delete single user
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult deleteEmployee(Guid id)
+        {
+            var userInDb = dbContext.Employees.Find(id);
+            if (userInDb is null)
+            {
+                return NotFound();
+            }
+            dbContext.Employees.Remove(userInDb);
+            dbContext.SaveChanges();
+            return Ok(userInDb);
+        }
+    }
+}
---
+++ CrudOnMySQL/Migrations/20241004130158_initial migration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrudOnMySQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<int>(type: "int", nullable: true),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    Photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
